cb7f9c3a5cf1132f690fb484f20712443b522df9
exaon18 <exaontech@gamil.com>
2024-12-09 23:27:39 -0800

    gd

--- real/settings.py
+++ real/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-woufk@=n%&v#my0_ay74wll9)3@nbhm#(c#cdvyuyd75a)0ciy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
